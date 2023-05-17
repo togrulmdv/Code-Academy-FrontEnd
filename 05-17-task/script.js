ff838fbbbc6873e4dc24f1c3c898ba59881ed402
TogrullM <125910346+TogrullM@users.noreply.github.com>
2023-05-17 18:26:18 +0400

    task finished2

--- 05-17-task/script.js
+++ 05-17-task/script.js
@@ -1,40 +1,41 @@
 document.addEventListener("DOMContentLoaded", getData);
 
-function getData() {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-        .then(response => response.json())
-        .then(data => displayData(data))
-        .catch(error => console.log(error));
+async function getData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayData(data) {
-    const userList = document.getElementById("userList");
+  const userList = document.getElementById("userList");
 
-    // User'lar üçün variable yaradır
-    const users = {};
-    data.forEach(item => {
-        if (!users[item.userId]) {
-            users[item.userId] = [];
-        }
-        users[item.userId].push(item);
+  const users = {};
+
+  data.forEach(item => {
+    const { userId, title } = item;
+    users[userId] = users[userId] || [];
+    users[userId].push(title);
+  });
+
+  for (const userId in users) {
+    const tasks = users[userId];
+    const userItem = document.createElement("li");
+    const userHeading = document.createElement("h2");
+    userHeading.textContent = "User " + userId;
+    userItem.appendChild(userHeading);
+
+    const taskList = document.createElement("ul");
+    tasks.forEach(title => {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = title;
+      taskList.appendChild(taskItem);
     });
 
-    // User'ları və task'larını display edir
-    for (const userId in users) {
-        const tasks = users[userId];
-        const userItem = document.createElement("li");
-        const userHeading = document.createElement("h2");
-        userHeading.textContent = "User " + userId;
-        userItem.appendChild(userHeading);
-
-        const taskList = document.createElement("ul");
-        tasks.forEach(task => {
-            const taskItem = document.createElement("li");
-            taskItem.textContent = task.title;
-            taskList.appendChild(taskItem);
-        });
-
-        userItem.appendChild(taskList);
-        userList.appendChild(userItem);
-    }
+    userItem.appendChild(taskList);
+    userList.appendChild(userItem);
+  }
 }

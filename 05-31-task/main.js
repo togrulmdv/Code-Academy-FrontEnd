99db7536561b246bc58d9ddcdafabbc293bbe0de
Togrul Mammadov <togrull.mammadov@gmail.com>
2023-05-31 00:43:37 +0400

    task finish

---
+++ 05-31-task/main.js
@@ -0,0 +1,25 @@
+function updateCountdown(minutes) {
+    var totalSeconds = minutes * 60;
+
+    var countdownInterval = setInterval(() => {
+        var days = Math.floor(totalSeconds / (60 * 60 * 24));
+        var hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+        var mins = Math.floor((totalSeconds % (60 * 60)) / 60);
+        var secs = Math.floor(totalSeconds % 60);
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = mins;
+        document.getElementById("seconds").textContent = secs;
+
+        if (totalSeconds <= 0) {
+            clearInterval(countdownInterval);
+        }
+
+        totalSeconds--;
+    }, 1000);
+}
+
+updateCountdown(10);
+
+
